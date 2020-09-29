ec6863fc83680399c34e504a9410a34f0b8e8121
Emon Khan <bdemon00@gmail.com>
2020-09-29 13:58:17 +0600

    Dashboard Counter added

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Agreement;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Payment;
+use App\Property;
+use App\Tent;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $counter = [];
+        $counter['properties'] = Property::count();
+        $counter['tents'] = Tent::count();
+        $counter['agreements'] = Agreement::count();
+        $counter['payments'] = Payment::count();
+        return view('admin.dashboard', compact('counter'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,9 +9,9 @@
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Sales</h5>
+                <h5 class="text-muted">Properties</h5>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">$12099</h1>
+                    <h1 class="mb-1">{{ $counter['properties'] }}</h1>
                 </div>
                 
             </div>
@@ -26,9 +26,9 @@
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">New Customer</h5>
+                <h5 class="text-muted">Tent</h5>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">1245</h1>
+                    <h1 class="mb-1">{{ $counter['tents'] }}</h1>
                 </div>
                 
             </div>
@@ -43,9 +43,9 @@
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Visitor</h5>
+                <h5 class="text-muted">Agreements</h5>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">13000</h1>
+                    <h1 class="mb-1">{{ $counter['agreements'] }}</h1>
                 </div>
                 
             </div>
@@ -60,9 +60,9 @@
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Total Orders</h5>
+                <h5 class="text-muted">Payments</h5>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">1340</h1>
+                    <h1 class="mb-1">{{ $counter['payments'] }}</h1>
                 </div>
                
             </div>
