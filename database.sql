873395a3a8689f7ce441ee643f88610a5edf5cfb
Emon Khan <bdemon00@gmail.com>
2020-10-12 16:37:30 +0600

    Sample Database Added

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2020 at 07:19 PM
+-- Generation Time: Oct 12, 2020 at 10:16 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -40,6 +40,13 @@ CREATE TABLE `agreements` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `agreements`
+--
+
+INSERT INTO `agreements` (`id`, `name`, `user_id`, `property_id`, `tent_id`, `advance`, `yearly_percent`, `attachment`, `created_at`, `updated_at`) VALUES
+(1, 'House Rent Agreement', 1, 1, 1, 50000, 12, 'agreement/fCwxnlscKwY95DwIu7UCnxTqUqKvyaqnAFcz53F3.png', '2020-09-30 18:00:00', '2020-10-11 01:44:10');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +55,10 @@ CREATE TABLE `agreements` (
 
 CREATE TABLE `borrows` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `entry` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -87,6 +98,12 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `loans` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `amount` int(11) NOT NULL,
+  `return_amount` int(11) NOT NULL,
+  `return_date` datetime NOT NULL,
+  `entry` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -112,14 +129,14 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2020_09_24_160510_create_expences_table', 1),
-(5, '2020_09_24_160525_create_loans_table', 1),
 (6, '2020_09_24_174423_create_transactions_table', 1),
 (7, '2020_09_25_055803_create_properties_table', 1),
 (8, '2020_09_25_055954_create_tents_table', 1),
 (9, '2020_09_25_060052_create_agreements_table', 1),
 (10, '2020_09_25_181053_create_types_table', 1),
-(11, '2020_09_26_105926_create_borrows_table', 1),
-(12, '2020_09_28_164711_create_payments_table', 1);
+(16, '2020_10_06_094204_create_borrows_table', 3),
+(17, '2020_09_24_160525_create_loans_table', 4),
+(19, '2020_09_28_164711_create_payments_table', 5);
 
 -- --------------------------------------------------------
 
@@ -143,13 +160,31 @@ CREATE TABLE `payments` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `agreement_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `month` int(11) DEFAULT NULL,
   `method` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `amount` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tnxid` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gst` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bank` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `account` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `remarks` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `branch` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cheque` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `attachment` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `agreement_id`, `user_id`, `type`, `month`, `method`, `amount`, `tnxid`, `gst`, `bank`, `account`, `branch`, `cheque`, `attachment`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'rent', 1, 'cash', '10000', '5f82be216bfb8', NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-11 02:11:13', '2020-10-11 02:11:13'),
+(2, 1, 1, 'rent', 2, 'cash', '10000', '5f82bf0237ccb', NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-11 02:14:58', '2020-10-11 02:14:58'),
+(3, 1, 1, 'rent', 3, 'cash', '10000', '5f82bf21795e8', NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-11 02:15:29', '2020-10-11 02:15:29'),
+(4, 1, 1, 'rent', 4, 'cash', '10000', '5f82c0ac72ea0', NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-11 02:22:04', '2020-10-11 02:22:04'),
+(5, 1, 1, 'rent', 5, 'cash', '10000', '5f82c0e95372f', NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-11 02:23:05', '2020-10-11 02:23:05');
 
 -- --------------------------------------------------------
 
@@ -169,6 +204,13 @@ CREATE TABLE `properties` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`id`, `type_id`, `name`, `rate`, `district`, `street`, `city`, `country`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Home', 10000, 'Dhaka', 'Ashulia', 'Dhaka', 'Bangladesh', '2020-10-09 18:00:00', '2020-10-10 10:14:32');
 
 -- --------------------------------------------------------
 
@@ -212,6 +254,13 @@ CREATE TABLE `tents` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `tents`
+--
+
+INSERT INTO `tents` (`id`, `fname`, `lname`, `cnic`, `cnica`, `address`, `city`, `country`, `contact1`, `contact2`, `contact3`, `g1_fname`, `g1_lname`, `g1_cnic`, `g1_cnica`, `g1_address`, `g1_city`, `g1_country`, `g1_contact1`, `g1_contact2`, `g1_contact3`, `g2_fname`, `g2_lname`, `g2_cnic`, `g2_cnica`, `g2_address`, `g2_city`, `g2_country`, `g2_contact1`, `g2_contact2`, `g2_contact3`, `created_at`, `updated_at`) VALUES
+(1, 'Emon', 'Khan', '123456', NULL, 'Ashulia', 'dhaka', 'Bangladesh', '01995329555', NULL, NULL, 'Mamun', 'Khan', '123', NULL, 'Ashulia', 'dhaka', 'Bangladesh', '111', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-10-10 10:15:35', '2020-10-10 10:15:35');
+
 -- --------------------------------------------------------
 
 --
@@ -238,6 +287,13 @@ CREATE TABLE `types` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `name`, `type`, `created_at`, `updated_at`) VALUES
+(1, 'House', 'property', '2020-10-09 18:00:00', '2020-10-10 10:13:59');
+
 -- --------------------------------------------------------
 
 --
@@ -254,6 +310,13 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Super Admin', 'bdemon00@gmail.com', NULL, '$2y$10$uOX8aWrtyHZUF6ZS/L5o4.d.o/YwzP2wQc3ZPcz1xtnfMUs7Tc8Gy', 'DoxEtTFfxS1S62jJ9kJ67q8sUDCfEqczLMIfBr4vG1BoVRrvcC7AeY31aq1a', '2020-10-06 02:53:22', '2020-10-06 02:53:22');
 
 --
 -- Indexes for dumped tables
@@ -346,7 +409,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `agreements`
 --
 ALTER TABLE `agreements`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `borrows`
@@ -376,25 +439,25 @@ ALTER TABLE `loans`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `properties`
 --
 ALTER TABLE `properties`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tents`
 --
 ALTER TABLE `tents`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `transactions`
@@ -406,13 +469,13 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
