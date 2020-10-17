22e7e1bd7c8a1b66aaef36440601dedc31594d92
Emon Khan <bdemon00@gmail.com>
2020-10-17 21:12:20 +0600

    Partial dashboard ready

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,20 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Agreement;
+use App\Borrow;
+use App\Loan;
 use App\Payment;
 use App\Property;
 use App\Tent;
+use App\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $counter = [];
-        $counter['properties'] = Property::count();
-        $counter['tents'] = Tent::count();
-        $counter['agreements'] = Agreement::count();
-        $counter['payments'] = Payment::count();
-        return view('dashboard', compact('counter'));
+        $total = (object) [];
+        $value = (object) [];
+
+        $total->borrow = Borrow::count();
+        $value->borrow = Borrow::pluck('amount')->sum();
+
+        $total->employee = User::count();
+        $value->employee = 0;
+
+        $total->wellpart = 0;
+        $value->wellpart = 0;
+
+        $total->loan = Loan::count();
+        $value->loan = Loan::pluck('amount')->sum();
+
+        $total->return = Loan::count();
+        $value->return = Loan::pluck('amount')->sum();
+
+        $total->payment = Payment::count();
+        $value->payment = Payment::pluck('amount')->sum();
+
+        $total->refund = Loan::count();
+        $value->refund = Loan::pluck('amount')->sum();
+
+        $value->cash = 10000;
+
+        return view('dashboard', compact('total','value'));
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,74 +3,104 @@
 @section('content')
 
 <div class="row">
-    <!-- ============================================================== -->
-    <!-- sales  -->
-    <!-- ============================================================== -->
+
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Properties</h5>
+                <h4 class="text-muted text-uppercase">Borrow</h4>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">{{ $counter['properties'] }}</h1>
+                    <h2 class="mb-1">{{ $total->borrow }} | ${{ $value->borrow }}</h2>
                 </div>
-                
+                <div class="text-right"><a href="{{ route('borrow.index') }}">View</a></div>
+
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- end sales  -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- new customer  -->
-    <!-- ============================================================== -->
+
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Tent</h5>
+                <h4 class="text-muted text-uppercase">Employee</h4>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">{{ $counter['tents'] }}</h1>
+                    <h2 class="mb-1">{{ $total->employee }} | ${{ $value->employee }}</h2>
                 </div>
-                
+                <div class="text-right"><a href="{{ route('user.index') }}">View</a></div>
+
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- end new customer  -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- visitor  -->
-    <!-- ============================================================== -->
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Agreements</h5>
+                <h4 class="text-muted text-uppercase">Well Part</h4>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">{{ $counter['agreements'] }}</h1>
+                    <h2 class="mb-1">{{ $total->wellpart }} | ${{ $value->wellpart }}</h2>
                 </div>
-                
+                <div class="text-right"><a href="{{ route('borrow.index') }}">View</a></div>
+
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- end visitor  -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- total orders  -->
-    <!-- ============================================================== -->
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Payments</h5>
+                <h4 class="text-muted text-uppercase">Loan</h4>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">{{ $counter['payments'] }}</h1>
+                    <h2 class="mb-1">{{ $total->loan }} | ${{ $value->loan }}</h2>
                 </div>
-               
+                <div class="text-right"><a href="{{ route('loan.index') }}">View</a></div>
+
             </div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- end total orders  -->
-    <!-- ============================================================== -->
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+        <div class="card border-3 border-top border-top-primary">
+            <div class="card-body">
+                <h4 class="text-muted text-uppercase">Loan Return</h4>
+                <div class="metric-value d-inline-block">
+                    <h2 class="mb-1">{{ $total->return }} | ${{ $value->return }}</h2>
+                </div>
+                <div class="text-right"><a href="{{ route('loan.index') }}">View</a></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+        <div class="card border-3 border-top border-top-primary">
+            <div class="card-body">
+                <h4 class="text-muted text-uppercase">Rent Payment</h4>
+                <div class="metric-value d-inline-block">
+                    <h2 class="mb-1">{{ $total->payment }} | ${{ $value->payment }}</h2>
+                </div>
+                <div class="text-right"><a href="{{ route('payment.index') }}">View</a></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+        <div class="card border-3 border-top border-top-primary">
+            <div class="card-body">
+                <h4 class="text-muted text-uppercase">Rent Refund</h4>
+                <div class="metric-value d-inline-block">
+                    <h2 class="mb-1">{{ $total->refund }} | ${{ $value->refund }}</h2>
+                </div>
+                <div class="text-right"><a href="{{ route('borrow.index') }}">View</a></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+        <div class="card border-3 border-top border-top-primary">
+            <div class="card-body">
+                <h4 class="text-muted text-uppercase">Cash On Hand</h4>
+                <div class="metric-value d-inline-block">
+                    <h2 class="mb-1">${{ $value->cash }}</h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
