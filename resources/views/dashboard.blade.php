9a8263f5f5de93fa94ba9a2865d1df0a73de2e6a
Emon Khan <bdemon00@gmail.com>
2020-10-20 19:32:59 +0600

    Over all report view on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <div class="row">
-
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
@@ -100,7 +99,46 @@
             </div>
         </div>
     </div>
+</div>
 
+<div class="row">
+    <div class="card col-12">
+        <h5 class="card-header">Over All Reports</h5>
+        <div class="card-body">
+            <table id="example" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Status</th>
+                        {{-- <th scope="col">Action</th> --}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (App\Type::all() as $type)
+                    <tr>
+                        <th scope="row">{{$type->id}}</th>
+                        <td>{{$type->name}}</td>
+                        <td>{{ $type->created_at->format('d-m-Y') }}</td>
+                        <td>{{$type->deleted_at ? 'Deleted':'Active'}}</td>
+                        {{-- <td class="text-right">
+                            <a href="{{ route('type.edit', $type->id)}}" class="btn btn-sm btn-warning"><i
+                                    class="fas fa-edit"></i></a>
+                            <form class="d-inline" action="{{route('type.destroy', $type->id)}}"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger"><i
+                                        class="fas fa-trash-alt"></i></button>
+                            </form>
+                        </td> --}}
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 
