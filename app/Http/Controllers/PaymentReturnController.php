c8d4f9ee48b572bf553df49f360e89e63b996a06
Emon Khan <bdemon00@gmail.com>
2020-10-21 23:15:31 +0600

    Create Payment return mmc

---
+++ app/Http/Controllers/PaymentReturnController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PaymentReturn;
+use Illuminate\Http\Request;
+
+class PaymentReturnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PaymentReturn  $paymentReturn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaymentReturn $paymentReturn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PaymentReturn  $paymentReturn
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaymentReturn $paymentReturn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PaymentReturn  $paymentReturn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaymentReturn $paymentReturn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PaymentReturn  $paymentReturn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaymentReturn $paymentReturn)
+    {
+        //
+    }
+}
